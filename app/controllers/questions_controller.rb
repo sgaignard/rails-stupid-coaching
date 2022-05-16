e5b85f3ae124092528adf4683c3b463af61288f2
shane gaignard <shane.c.gaignard@gmail.com>
2022-05-16 14:24:43 +0200

    refacto

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,8 +1,17 @@
 class QuestionsController < ApplicationController
   # top level comment
   def ask
+  end
 
   def answer
     @ask = params[:ask]
+
+    if @ask == 'I am going to work'
+      @answer = 'Great!'
+    elsif @ask.last == '?'
+      @answer = 'Silly Question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
   end
 end
